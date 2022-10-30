5027cecd1dc2b4d8871bdd45b0cb58ef4d3ddb62
SvoronaM <sergeymigal@gmail.com>
2022-10-30 21:47:26 +0100

    multer-config.js

--- back/middleware/multer-config.js
+++ back/middleware/multer-config.js
@@ -8,24 +8,13 @@ const MIME_TYPES = {
     'image/png': 'png',
     'image/webp': 'webp'
 };
-// Utilisation de la function diskStorage pour dire que l'on va enregistrer sur le disque
 const storage = multer.diskStorage({
     destination: (req, file, callback) => {
-        if (!fs.existsSync('images')) { /* si le dossier images n'existe pas */
-            fs.mkdirSync('images'); /* créé un dossier images */
-            callback(null, 'images'); /* le callback enregistre l'image sur images */
-        } else {
-        // Callback enregistre l'image sur images
-            callback(null, 'images');
-        }
+        callback(null, 'images');
     },
-// Filename qui explique à multer quel nom de fichier utiliser
     filename: (req, file, callback) => {
-    // Nouveau nom avec une séparation du nom original et qui les remplace par le _ en les join en 1 seul string
         const name = file.originalname.split(' ').join('_');
-        // Créer l'extension du fichier qui correspond au mimetype du fichier envoyé
         const extension = MIME_TYPES[file.mimetype];
-        // Appelle le callback avec le name du fichier auquel on ajoute le temps, puis un . et l'extension
         callback(null, name + Date.now() + '.' + extension);
     }
 });
