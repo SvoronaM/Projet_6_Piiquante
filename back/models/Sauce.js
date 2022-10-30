c3ca5486cd7ab309da41c551533f1c39bfb81a19
SvoronaM <sergeymigal@gmail.com>
2022-10-30 20:36:30 +0100

    app

--- back/models/Sauce.js
+++ back/models/Sauce.js
@@ -4,8 +4,15 @@ const mongoose = require("mongoose");
 const sauceSchema = mongoose.Schema({
     userId: { type: String, required: true },
     name: { type: String, required: true },
+    manufacturer: { type: String, required: true },
     description: { type: String, required: true },
+    mainPepper: { type: String, required: true },
     imageUrl: { type: String, required: true },
+    heat: { type: Number, required: true },
+    likes: { type: Number, required: true },
+    dislikes: { type: Number, required: true },
+    usersLiked: [{ type: String, required: true }],
+    usersDisliked: [{ type: String, required: true }],
 }); 
 // Export du model terminé grace à mongoose.model(avec le nom du model, schema du model)
 module.exports = mongoose.model("Sauce", sauceSchema);
